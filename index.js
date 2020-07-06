09bcff489a90b8dd0214341c1d293b63d664f467
Feanor1992 <feanor1992@gmail.com>
2020-07-06 17:19:34 +0300

    some change in html and styles. add buttons in slider

--- index.js
+++ index.js
@@ -42,11 +42,7 @@ function generateGallery() {
 
     //generate slider
     let figures = document.querySelectorAll('figure');
-    for (let figure of figures) {
-        figure.addEventListener("click", () => {
-            generateSlider(figure);
-        });
-    }
+    figures.forEach(e => e.addEventListener('click', () => generateSlider(e)));
 
     function generateSlider(figure) {
         darken();
@@ -65,20 +61,83 @@ function generateGallery() {
         }
 
         for (let figure of figures) {
-            if (document.querySelector('.opacity') === null) {
-                figure.classList.add('opacity');
+            if (figure.hasAttribute("style")) {
+                figure.removeAttribute("style");
             } else {
-                figure.classList.remove('opacity');
+                figure.setAttribute("style", "margin: 0; width: auto; position: absolute; opacity: 0;");
+            }
+        }
+        
+        //generate buttons in slider and change opacity
+        //to choosen element
+        if (figure.hasAttribute("style")) {
+            figure.style.opacity = 1;
+            generateButtons();
+        } else generateButtons();
+
+        function generateButtons() {
+            if (document.querySelector(".buttons") === null) {
+                let buttons = document.createElement("div");
+                buttons.classList.add('buttons');
+                gallery.appendChild(buttons);
+
+                let leftButton = document.createElement("button");
+                leftButton.classList.add("button", "left-button");
+                let leftImg = document.createElement("img");
+                leftImg.src = "img/pointing-left.svg";
+                leftButton.appendChild(leftImg);
+                buttons.appendChild(leftButton);
+                leftButton.addEventListener('click', () => slideChanging('-'));
+
+                let rightButton = document.createElement("button");
+                rightButton.classList.add("button", "right-button");
+                let rightImg = document.createElement("img");
+                rightImg.src = "img/pointing-right.svg";
+                rightButton.appendChild(rightImg);
+                buttons.appendChild(rightButton);
+                rightButton.addEventListener('click', () => slideChanging('+'));
+
+                let closeButton = document.createElement("button");
+                closeButton.classList.add("button", "close-button");
+                let closedImg = document.createElement("img");
+                closedImg.src = "img/closed.svg";
+                closeButton.appendChild(closedImg);
+                buttons.appendChild(closeButton);
+                closeButton.addEventListener("click", () => generateSlider(figure));
+            } else {
+                let buttons = document.querySelector('.buttons');
+                gallery.removeChild(buttons);
             }
         }
 
-    }
-    
+        function slideChanging(e) {
+            figures.forEach(e => e.style.opacity = 0);
 
-    //generate buttons in slider and change opacity
-    //to choosen element
-    if (figures.classList.contains('.opacity') === true) {
-        figures.style.opacity = 1;
-        generateButtons();
-    } else generateButtons();
+            if (e === '-') {
+                if (figure === figures[0]) {
+                    figure = figures[figures.length - 1];
+                } else {
+                    figure = figure.previousElementSibling;
+                }
+            } else if (e === '+') {
+                if (figure === figures[figures.length - 1]) {
+                    figure = figures[0];
+                } else {
+                    figure = figure.nextElementSibling;
+                }
+            }
+            figure.style.opacity = 1;
+        }
+
+        //changing slides by keyboard
+        document.addEventListener("keydown", (e) => {
+            if (e.keyCode === 37 || e.keyCode === 189) {
+              slideChanging("-");
+            } else if (e.keyCode === 39 || e.keyCode === 187) {
+              slideChanging("+");
+            } else if (e.keyCode === 27) {
+              generateSlider(figure);
+            }
+        });
+    }
 }
